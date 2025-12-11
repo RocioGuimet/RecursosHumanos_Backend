85577feab0cdbf923412c855cb1a2fff7db014b0
unknown <rocioguimet@hotmail.com>
2025-12-11 16:03:42 -0300

    Buscar Por Id con React y Axios (Editar)

--- RecursosHumanosReact/src/empleados/EditarEmpleado.js
+++ RecursosHumanosReact/src/empleados/EditarEmpleado.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function EditarEmpleado() {
+
+    const urlBase = "http://localhost:8080/rh-app/empleados";
+
     let Navegacion = useNavigate();
+
+    const {id} = useParams();
 
     const [empleado, setEmpleado]=useState({
         nombre:"",
@@ -13,6 +18,16 @@ export default function EditarEmpleado() {
 
     const {nombre, departamento, sueldo} = empleado
 
+    useEffect(()=>{
+        cargarEmpleado();
+
+    },[])
+    
+    const cargarEmpleado = async () => {
+        const resultado = await axios.get(`${urlBase}/${id}`)
+        setEmpleado(resultado.data); 
+    }
+
     const onInputChange = (e) => {
         //Spread Operator ... (expandir atributos)
         setEmpleado({...empleado, [e.target.name]: e.target.value})
@@ -20,7 +35,6 @@ export default function EditarEmpleado() {
     //Petición al Backend
     const onSubmit = async (e) => {
         e.preventDefault();
-        const urlBase = "http://localhost:8080/rh-app/empleados";
         await axios.post(urlBase, empleado);
         //Redirigimos a la página de inicio
         Navegacion('/');
